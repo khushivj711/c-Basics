e6aeec422b4d5ba1aac1608087c7800ca2f6421d
khushivj711 <khushi.vijay8711@gmail.com>
2024-09-04 19:59:35 +0000

    doing typecasting and operator

---
+++ typecasting_operator.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+int main(){
+  // int a = 'a';
+  // cout<<a<<endl;
+
+  // char ch = 98;
+  // cout<<ch<<endl;
+
+  // // char ch1 = 123456;
+  // // cout<< ch1<<endl;
+
+  // unsigned int b = -112;
+  // cout<< b <<endl;
+
+  // int a1 = 2;
+  // int b1 = 3;
+  // bool first = (a1 ==  b1);
+  // cout<<first<<endl;
+
+  // bool second = (a1 >  b1);
+  // cout<<second<<endl;
+
+  // bool third = (a1 <  b1);
+  // cout<<third<<endl;
+
+  // bool fourth = (a1 >= b1);
+  // cout<<fourth<<endl;
+
+  // bool fifth = (a1 <=  b1);
+  // cout<<fifth<<endl;
+
+  // bool sixth = (a1 !=  b1);
+  // cout<<sixth<<endl;
+
+  //logical operator = && ,|| , !
+
+  int a = 21;
+  cout<<!a<<endl;
+
+  cout<<7/3;
+}
