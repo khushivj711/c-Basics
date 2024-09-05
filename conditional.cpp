af25fe70f830f7bbf242b95d42bb3026c991a448
khushivj711 <khushi.vijay8711@gmail.com>
2024-09-05 17:41:04 +0000

    conditional statements and find outputs

---
+++ conditional.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+int main(){
+  // int n ;
+  // cin>>n;
+  // cout<<"hello"<<endl;
+  // cout<<"The value of n is : " << n << endl;
+
+  // int a ;
+  // cin>>a;
+  // if(a>0){
+  //   cout<<"a is positive :"<<a<<endl;
+  // }
+  // else{
+  //   cout<<"a is negative :"<<a<<endl;
+  // }
+
+  // int a , b;
+  // cin>>a>>b;
+  // if(a>b){
+  //   cout<<"a is greater than b :"<<a<<endl;
+  // }
+  // else{
+  //   cout<<"b is greater than a :"<<b<<endl;
+  // }
+
+  //cin function cannot read the space , tab, enter etc. but cin.get() read 
+
+  //cin.get() function is used to take input but provide ascii value of character 
+  // int a = cin.get();
+  // cout<<"value of a is :"<< a << endl;
+
+
+  //if-if 
+//   int a , b;
+
+//   cout<<"Enter the value of a :"<<endl;
+//   cin >>a ;
+//   cout<<"Enter the value of b :"<<endl;
+//   cin>>b;
+
+  // if(a>b){
+  //   cout<<"a is greater than b :"<<a<<endl;
+  // }
+  // if(b>a){
+  //   cout<<"b is greater than a :"<<b<<endl;
+  // }
+
+
+//   if(a>0){
+//     cout<<"a is positive"<<endl;
+//   }
+//   else if(a<0){
+//     cout<<"a is negative"<<endl;
+//   }
+//  else{
+//    cout<<"a is zero"<<endl;
+//  }
+  
+
+int a = 2;
+int b = a+1;
+
+if((a=3)==9){
+    cout<<a;
+}
+else{
+    cout<<a+1;
+}
+}
