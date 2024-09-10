ffa53872f647f35e3ddce0007b271e540378ae30
khushivj711 <khushi.vijay8711@gmail.com>
2024-09-10 14:15:25 +0000

    solve more questions

--- pattern1.cpp
+++ pattern1.cpp
@@ -171,8 +171,156 @@ int main(){
 //     cout<<endl;
 //     i++;
 
+ // int i = 1;
+  // char ch = 'A';
+  // while(i<=n){
+  //   int j = 1;
+  //   while(j<=i){
+  //     cout<<ch<<' ';
+  //     ch++;
+  //     j++;
+  //   }
+  //   cout<<endl;
+  //   i++;
+  // }
 
-  }
+  // int i = 1;
+  // while(i<=n){
+  //   int j = 1;
+  //   char ch ='A'+i+j-2 ;
+  //   while(j<=i){
+  //     cout<<ch<<" ";
+  //     ch++;
+  //     j++;
+  //   }
+  //   cout<<endl;
+  //   i++;
+  // }
+
+  // int i = 1;
+  // while(i<=n){
+
+  //   int j = 1;
+  //   char ch = 'A'+n-1;
+  //   while(j<=i){
+  //     cout<<ch<<' ';
+  //     ch--;
+  //     j++;
+  //   }
+  //   cout<<endl;
+  //   i++;
+  // }
+
+  // int i = 1;
+  // while(i<=n){
+
+  //   int j = 1;
+  //   char ch = 'A'+n-i;
+  //   while(j<=i){
+  //     cout<<ch<<' ';
+  //     ch++;
+  //     j++;
+  //   }
+  //   cout<<endl;
+  //   i++;
+  // }
+
+  // int i = 1;
+  // while(i<=n){
+  //   int j = 1;
+  //   char ch = 'A'+i-1;
+  //   while(j<=n){
+  //     cout<<ch<<' ';
+  //     ch++;
+  //     j++;
+  //   }
+  //   cout<<endl;
+  //   i++;
+  // }
+
+  // int i = 1;
+  // while (i <= n) {
+  //   int k=1;
+  //   while(k<=n-i){
+  //     cout<<" "<<" ";
+  //     k++;
+  //   }
+  //   int j = 1;
+  //   while(j<=i){
+  //     cout<<"*"<<" ";
+  //     j++;
+  //   }
+  //   cout<<endl;
+  //   i++;
+  // }
+
+  // int i = 1;
+  // while(i<=n){
+
+  //   int j = 1;
+  //   while(j<=n-i+1){
+  //     cout<<"*"<<" ";
+  //     j++;
+  //   }
+  //   cout<<endl;
+  //   i++;
+  // }
+
+  
+  // int i = 1;
+  // while(i<=n){
+
+  //   int k = 1;
+  //   while(k<=i-1){
+  //     cout<<" "<<" ";
+  //     k++;
+  //   }
+    
+  //   int j = 1;
+  //   while(j<=n-i+1){
+  //     cout<<"*"<<" ";
+  //     j++;
+  //   }
+  //   cout<<endl;
+  //   i++;
+  // }
+
+  // int i = 1;
+  // while(i<=n){
+
+  //   int k = 1;
+  //   while(k<=i-1){
+  //     cout<<" "<<" ";
+  //     k++;
+  //   }
+
+  //   int j = 1;
+  //   while(j<=n-i+1){
+  //     cout<<i<<" ";
+  //     j++;
+  //   }
+  //   cout<<endl;
+  //   i++;
+  // }
+
+  // int i = 1;
+  // while(i<=n){
+
+  //   int k = 1;
+  //   while(k<=n-i){
+  //     cout<<" "<<" ";
+  //     k++;
+  //   }
+
+  //   int j = 1;
+  //   while(j<=i){
+  //     cout<<i<<" ";
+  //     j++;
+  //   }
+  //   cout<<endl;
+  //   i++;
+  // }
+  
 
   
 
