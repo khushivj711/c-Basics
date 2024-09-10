47447024364bb21ce763c7ad1ec33590cdfe6af5
khushivj711 <khushi.vijay8711@gmail.com>
2024-09-10 13:17:30 +0000

    solution of pattern question

---
+++ pattern1.cpp
@@ -0,0 +1,180 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int n ;
+  cin>>n;
+  // int i = 1;
+  // while(i<=n){
+  //   int j = 1;
+  //   while(j<=n){
+  //     cout<<j;
+  //     j++;
+  //   }
+  //   cout<<endl;
+  //   i++;
+  // }
+
+  // int i = 1;
+  // while(i<=n){
+  //   int j = 1;
+  //   while(j<=n){
+  //     cout<<n-j+1;
+  //     j++;
+  //   }
+  //   cout<<endl;
+  //   i++;
+  // }
+
+  
+  // int count = 1;
+  // while(i<=n){
+  //   int j=1;
+  //   while(j<=n){
+  //     cout<<count<<" ";
+  //     count++;
+  //     j++;
+  //   }
+  //   cout<<endl;
+  //   i++;
+  // }
+
+  
+  // int i = 1;
+  // while(i<=n){
+  //   int j = 1;
+  //   while(j<=i){
+  //     cout<<"*"<<" ";
+  //     j++;
+  //   }
+  //   cout<<endl;
+  //   i++;
+  // }
+
+
+  // int i = 1;
+  // while(i<=n){
+  //   int j = 1;
+  //   while(j<=i){
+  //     cout<<i<<" ";
+  //     j++;
+  //   }
+  //   cout<<endl;
+  //   i++;
+  // }
+
+  
+  // int i = 1;
+  // int count= 1;
+  // while(i<=n){
+  //   int j = 1;
+  //   while(j<=i){
+  //     cout<<count<<" ";
+  //     count++;
+  //     j++;
+  //   }
+  //   cout<<endl;
+  //   i++;
+  // }
+
+
+  // int i = 1;
+  // while(i<=n){
+  //   int j = 1;
+  //   int count= i;
+  //   while(j<=i){ 
+  //     cout<<count<<" ";
+  //     count++;
+  //     j++;
+  //   }
+  //   cout<<endl;
+  //   i++;
+  // }
+
+  
+  // int i = 1;
+  // while(i<=n){
+  //   int j = 1;
+  //   int count= i;
+  //   while(j<=i){ 
+  //     cout<<count<<" ";
+  //     count--;
+  //     j++;
+  //   }
+  //   cout<<endl;
+  //   i++;
+  // }
+
+
+  // int i = 1;
+  // while(i<=n){
+  //   int j = 1;
+  //   while(j<=i){ 
+  //     cout<<i-j+1<<" ";
+  //     j++;
+  //   }
+  //   cout<<endl;
+  //   i++;
+  // }
+
+
+  // int i = 1;
+  // char character = 'A';
+  // while(i<=n){
+  //   int j = 1;
+  //   while(j<=n){
+  //     cout<<character<<' ';
+  //     j++;
+  //   }
+  //   character++;
+  //   cout<<endl;
+  //   i++;
+  // }
+
+
+  // int i = 1;
+  // while(i<=n){
+  //   int j = 1;
+  //   char character = 'A';
+  //   while(j<=n){
+  //     cout<<character<<' ';
+  //     character++;
+  //     j++;
+  //   }
+  //   cout<<endl;
+  //   i++;
+  // }
+
+
+  // int i = 1;
+  // while(i<=n){
+  //   int j = 1;
+  //   char ch = char(64 + i +j -1) ;
+  //   while(j<=n){
+  //     cout<<ch<<' ';
+  //     ch++;
+  //     j++;
+  //   }
+  //   cout<<endl;
+  //   i++;
+  // } 
+
+
+//   int i = 1;
+//   char ch = 'A';
+//   while(i<=n){
+//     int j  = 1;
+//     while(j<=i){
+//       cout<<ch<<' ';
+//       j++;
+//     }
+//      ch++;
+//     cout<<endl;
+//     i++;
+
+
+  }
+
+  
+
+
+}
