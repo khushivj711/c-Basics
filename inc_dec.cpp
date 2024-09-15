3283a4d0cf7714a9f13b565e9423becb6f995cce
khushivj711 <khushi.vijay8711@gmail.com>
2024-09-15 02:49:20 +0000

    basic quesitons on post/pre increment and decrement

---
+++ inc_dec.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // int a , b=1;
+    // a = 10;
+    // if(++a)
+    //     cout<<b<<endl;
+    // else
+    //     cout<<++b<<endl; 
+
+    // int a = 1;
+    // int b=2;
+
+    // if(a-- > 0 && ++b > 2){
+    //     cout<<"Stage1 - Inside If ";
+    // }  
+    // else{
+    //     cout<<"Stage2 - Inside else";
+    // }
+    // cout<<a<<" "<<b<<endl;
+
+
+    // int number = 3;
+    // cout<<(25 * (++number));
+    
+    int a = 1;
+    int b = a++;
+    int c = ++a;
+    cout<<b;
+    cout<<c;
+
+}
