a9dcb9673a571e7fa39004c904516766e672238d
khushivj711 <khushi.vijay8711@gmail.com>
2024-09-18 02:30:50 +0000

    using nested switch

--- Switch.cpp
+++ Switch.cpp
@@ -2,19 +2,38 @@
 using namespace std;
 
 int main(){
-    int num = 2;
+    int num = 1;
     char ch = '1';
 
     cout<<endl;
+
+    // switch (ch)
+    // {
+    // case 1: cout<<"First"<<endl;
+    //     /* code */
+    //     break;
+    // case '1' : cout<<"Second"<<endl;
+    //     break;
+    // default:   cout<<"It is default case"<<endl;
+        
+    //         }
+
 
     switch (ch)
     {
     case 1: cout<<"First"<<endl;
         /* code */
         break;
-    case '1' : cout<<"Second"<<endl;
+    case '1' : 
+                switch (num)
+                {
+                case 1: cout<<"Value of num is : "<<num<<endl;
+                    /* code */
+                    break;
+                }
         break;
     default:   cout<<"It is default case"<<endl;
         
             }
+
 }
